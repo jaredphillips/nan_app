daa119527b320e681ac4141ac027fad159dea25a
Jared Phillips <jaredtphillips@me.com>
2013-11-22 20:53:52 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NanApp::Application.config.secret_key_base = 'e86c0f9969e51a77278f80b60f90c6cf446ea0100df7202a9b07ec557fb9c01b759e6c95b3c047662b9dec927da6becad0537f3b40bb4f4d38c9542f3457a61b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NanApp::Application.config.session_store :cookie_store, key: '_nan_app_session'
