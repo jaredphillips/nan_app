aeda271fcadf36c7967028295dfb5240ace9e683
Jared Phillips <jaredtphillips@me.com>
2013-11-25 00:24:23 -0500

    heroku sucks

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -1,9 +1,8 @@
 class SubscriberMailer < ActionMailer::Base
-  default from: 'itsjaredphillips@gmail.com'
+  default from: 'notifications@example.com'
  
   def welcome_email(subscriber)
     @subscriber = subscriber
-    @url  = 'http://www.nan-emporium.com'
-    mail(to: @subscriber.email, subject: 'Welcome and thank you!')
+    mail(to: @subscriber.email, subject: 'Welcome')
   end
 end
