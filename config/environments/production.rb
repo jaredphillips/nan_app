ea5ed061b807ba0c00fa465b364ab6c9c552805d
Jared Phillips <jaredtphillips@me.com>
2013-11-24 19:03:16 -0500

    mail production settings tweaked

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,19 @@ NanApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'arcane-earth-7690.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "arcane-earth-7690.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["itsjaredphillips@gmail.com"],
+    password: ENV["sg7gf5k3W9Li86iy433fkxL1fTu81"]
+  }
 end
