e61203b23ac026a03e746bbefe4caf2e2f6700f5
Jared Phillips <jaredtphillips@me.com>
2013-11-24 23:38:48 -0500

    fixed subscribers folder and some things that dev enviro didn't catch

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -1,9 +1,9 @@
 class SubscriberMailer < ActionMailer::Base
   default from: 'test@example.com'
  
-  def welcome_email(user)
-    @user = user
+  def welcome_email(subscriber)
+    @subscriber = subscriber
     @url  = 'http://www.nan-emporium.com'
-    mail(to: @user.email, subject: 'Welcome and thank you!')
+    mail(to: @subscriber.email, subject: 'Welcome and thank you!')
   end
 end
