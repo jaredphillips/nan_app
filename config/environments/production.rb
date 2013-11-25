0533b7bbe0f4e4f5310e269edc1d044d5eed1f6c
Jared Phillips <jaredtphillips@me.com>
2013-11-24 19:47:01 -0500

    more email in production tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ NanApp::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+   config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
@@ -78,18 +78,18 @@ NanApp::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'arcane-earth-7690.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "arcane-earth-7690.herokuapp.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["itsjaredphillips@gmail.com"],
-    password: ENV["sg7gf5k3W9Li86iy433fkxL1fTu81"]
-  }
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'arcane-earth-7690.herokuapp.com',
+    :user_name            => 'itsjaredphillips@gmail.com',
+    :password             => 'sg7gf5k3W9Li86iy433fkxL1fTu81',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
